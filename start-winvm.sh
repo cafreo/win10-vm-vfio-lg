c23008fb7d40b72dd8db36c687d609f75f32c509
cafreo <taehyun_uses_tools.se9ou@slmail.me>
2023-09-28 18:21:06 +0200

    updated start script to try to attach controller to VM, when connected to host

--- start-winvm.sh
+++ start-winvm.sh
@@ -2,5 +2,5 @@
 #kitty -e sudo systemctl restart libvirtd
 #wait
 #sudo chmod 666 /dev/vfio/vfio &&
-kitty -e --title "Looking Glass win10VM" sudo -S virsh start win10;
+kitty -e --title "Looking Glass win10VM" sudo sh -c 'virsh start win10; bash /home/cafreo/Scripts/VM/xboxc-usb.sh attach';
 looking-glass-client
